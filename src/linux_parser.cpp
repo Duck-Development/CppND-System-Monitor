482f36a5770d7cc839b817acd31c75a456dd384f
Duck Development <rad.junki@gmail.com>
2020-10-24 20:14:28 +0200

    add user name in the user collom in processes view

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -205,13 +205,43 @@ string LinuxParser::Command(int pid [[maybe_unused]]) { return string(); }
 // REMOVE: [[maybe_unused]] once you define the function
 string LinuxParser::Ram(int pid [[maybe_unused]]) { return string(); }
 
-// TODO: Read and return the user ID associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid [[maybe_unused]]) { return string(); }
 
-// TODO: Read and return the user associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid [[maybe_unused]]) { return string(); }
+string LinuxParser::Uid(int pid){ 
+   string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory +to_string(pid) + kStatusFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      linestream >> key >>value;
+        if (key == "Name:") {
+          break;
+      } 
+    }
+  }
+  return value;
+ }
+
+string LinuxParser::User(int pid) {
+  auto uid = Uid(pid);
+  string line, tmp;
+  string key;
+  string value;
+  std::ifstream filestream(kPasswordPath);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::istringstream linestream(line);
+      linestream >>value >> tmp >> key;
+        if (key == uid) {
+          break;
+      } 
+    }
+  }
+  return value;
+
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
--- src/system.cpp
+++ src/system.cpp
@@ -19,8 +19,15 @@ using std::vector;
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+    processes_.clear();
+    auto pids = LinuxParser::Pids();
+    for (auto &pid : pids)
+    {
+        processes_.push_back(Process(pid));
+    }
+    return processes_; 
+}
 
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
